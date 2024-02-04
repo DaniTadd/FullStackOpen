387009dde2b76630eb2236d852a2cbbf02e79be8
Daniel Taddia <dtaddia@laboratoriosbernabo.com>
2024-02-04 00:51:27 -0300

    Exercises 1.1 and 1.2 completed

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,43 +1,70 @@
-/* import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Header = ({course}) => <h1>{course}</h1>
 
+const Content1 = ({coursesInfo}) => {
+  const array = coursesInfo.map ((coursesInfo) => <li key={coursesInfo.partNumber}> {coursesInfo.title}, number of exercises: {coursesInfo.exercises} </li>)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h2>Content for exercise 1.1</h2>
+    <ul>
+      {array}
+    </ul>
+    </>
+  )
+}
+const Part = ({coursesInfo}) => {
+  
+ return (
+  <>
+    <li > The course {coursesInfo.title} has {coursesInfo.exercises} number of excercises</li>
+  </>
+ )
+}
+
+
+
+
+const Content2 = ({coursesInfo}) => {
+  
+    return (
+    <>
+    <h2>Content for exercise 1.2</h2>
+    <ul>
+      <Part coursesInfo={coursesInfo[0]}/>
+      <Part coursesInfo={coursesInfo[1]}/>
+      <Part coursesInfo={coursesInfo[2]}/>
+    </ul>
     </>
   )
 }
 
-export default App */
+const Total = ({coursesInfo}) => {
+  let sum = 0
+  coursesInfo.forEach(coursesInfo => 
+    {sum = sum + (coursesInfo.exercises)
+  });
+    return <p>Total number of exercises: {sum}</p>
+  }
+
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const coursesInfo = [
+    {partNumber: 1, title: 'Fundamentals of React', exercises: 10},
+    {partNumber: 2, title: 'Using props to pass data', exercises: 7},
+    {partNumber: 3, title: 'State of a component', exercises: 14},
+  ]
   return (
     <div>
-      <p>Hello world</p>
+      <h1>Exercise 1.1</h1>
+      <Header course = {course}/>
+      <Content1 coursesInfo = {coursesInfo}/>
+      <Total coursesInfo = {coursesInfo}/>
+
+      <h2>Exercise 1.2</h2>
+      <Header course = {course}/>
+      <Content2 coursesInfo = {coursesInfo}/>
+      <Total coursesInfo = {coursesInfo}/>
     </div>
   )
 }
